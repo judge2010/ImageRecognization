0622d432c3a154ba47b7e61c7d1492bde6fce88b
judge2010 <74088083+judge2010@users.noreply.github.com>
2021-01-23 13:08:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,23 @@ function tphoto(){
         document.getElementById("photo").innerHTML = "<img src='"+data_uri+"' id='snapshot'>";
     });
 };
+console.log(ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/l4PUfUiRk/model.json',modelloaded);
+function modelloaded(){
+ console.log("model is loaded");
+
+};
+function Idenphoto(){
+ img = document.getElementById("snapshot");
+ classifier.classify(img, gotResult);
+};
+function gotResult(error,result){
+ if(error){
+     console.error(error);
+ }
+ else{
+     console.log(result);
+     document.getElementById("object").innerHTML = result[0].label;
+     document.getElementById("acuracy").innerHTML = result[0].confidence.toFixed(2);
+ };
+};
